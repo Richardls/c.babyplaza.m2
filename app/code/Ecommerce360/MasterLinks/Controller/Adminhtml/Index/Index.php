befdc00b4f230fdc9c441de63780238f9189087c
Richard Landa <richardlanda@gmail.com>
2025-04-08 23:46:37 -0500

    Nuevo modulo Masterlinks

--- app/code/Ecommerce360/MasterLinks/Controller/Adminhtml/Index/Index.php
+++ app/code/Ecommerce360/MasterLinks/Controller/Adminhtml/Index/Index.php
@@ -1,28 +1,22 @@
 <?php
 namespace Ecommerce360\MasterLinks\Controller\Adminhtml\Index;
 
-use Magento\Backend\App\Action;
-use Magento\Framework\Controller\ResultFactory;
-
-class Index extends Action
+class Index extends \Magento\Backend\App\Action
 {
-    // Debe coincidir con tu resource de ACL (ver punto 3)
-    const ADMIN_RESOURCE = 'Ecommerce360_MasterLinks::masterlinks_view';
+    protected $resultPageFactory;
 
-    /**
-     * Muestra la página principal del grid de MasterLinks
-     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
     public function execute()
     {
-        // Crear la página
-        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
-
-        // Ajustar el título
+        $resultPage = $this->resultPageFactory->create();
         $resultPage->getConfig()->getTitle()->prepend(__('Manage Master Links'));
-
-        // Si deseas que se resalte el menú
-        $resultPage->setActiveMenu('Ecommerce360_MasterLinks::main_menu_masterlinks');
-
         return $resultPage;
     }
 }
