094febfa154b3e7a881d1e161103f0de0c1d6dc8
Richard Landa <richardlanda@gmail.com>
2025-04-08 23:01:55 -0500

    Nuevo modulo Masterlinks

---
+++ app/code/Ecommerce360/MasterLinks/Model/ResourceModel/MasterLink/Grid/Collection.php
@@ -0,0 +1,66 @@
+<?php
+namespace Ecommerce360\MasterLinks\Model\ResourceModel\MasterLink\Grid;
+
+use Ecommerce360\MasterLinks\Model\ResourceModel\MasterLink\Collection as MasterLinkCollection;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Document as MasterLinkModel;
+use Magento\Framework\Api\Search\SearchResultInterface;
+
+class Collection extends MasterLinkCollection implements SearchResultInterface
+{
+    protected $aggregations;
+
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        $mainTable,
+        $eventPrefix,
+        $eventObject,
+        $resourceModel,
+        $model = MasterLinkModel::class,
+        $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
+        $this->_eventPrefix = $eventPrefix;
+        $this->_eventObject = $eventObject;
+        $this->_init($model, $resourceModel);
+        $this->setMainTable($mainTable);
+    }
+
+    public function getAggregations()
+    {
+        return $this->aggregations;
+    }
+    
+    public function setAggregations($aggregations)
+    {
+        $this->aggregations = $aggregations;
+    }
+    
+    public function getSearchCriteria()
+    {
+        return null;
+    }
+    
+    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
+    {
+        return $this;
+    }
+    
+    public function getTotalCount()
+    {
+        return $this->getSize();
+    }
+    
+    public function setTotalCount($totalCount)
+    {
+        return $this;
+    }
+    
+    public function setItems(array $items = null)
+    {
+        return $this;
+    }
+}
